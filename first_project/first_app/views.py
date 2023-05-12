858fb8b63db9e897b5cecc63c843e068548fdef8
DHRUV-V-S <83204811+DHRUV-V-S@users.noreply.github.com>
2023-05-12 14:27:38 +0530

    Add files via upload

---
+++ first_project/first_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# 'request' name is convention. It can be some other name too.
+def index(request) :    
+  return render(request, 'index.html')
